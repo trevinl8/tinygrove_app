6a4e6ba837cd545e2da71f2db2e0c5952f5cd4bb
Trevin Landers <trevin_landers@hotmail.com>
2024-12-11 17:08:49 +0000

    Sign Up Page

---
+++ android/app/src/main/kotlin/com/example/tinygrove_android_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tinygrove_android_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+class Config {
+  static String key = "";
+  static String secret = "";
+  static String url = "https://tinygroveuk.live-website.com/wp-json/wc/v3/";
+  static String customerURL = "customers";
+}
